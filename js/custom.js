3f2cef4ae6019ece3f4e2c2b77446dd864ebda9e
shargis21 <shargis21@gmail.com>
2017-03-13 21:11:31 -0400

    working on map

--- js/custom.js
+++ js/custom.js
@@ -35,3 +35,103 @@ $(document).ready(function(){
   });
 })
 
+// The Map
+
+$(document).ready(function() {
+
+
+// Satellite Map
+
+var satelliteLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiaml1bnR3YW5nIiwiYSI6ImNpeWs5OWVvMjAwMHYyd25yNnh5b2wzZHUifQ.ZIXQ4cl0l5C5UNYuuJtTzw', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'your.mapbox.project.id',
+    accessToken: 'pk.eyJ1Ijoiaml1bnR3YW5nIiwiYSI6ImNpeWs5OWVvMjAwMHYyd25yNnh5b2wzZHUifQ.ZIXQ4cl0l5C5UNYuuJtTzw'
+});
+
+
+//Default Map
+
+var defaultLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiaml1bnR3YW5nIiwiYSI6ImNpeWs5OWVvMjAwMHYyd25yNnh5b2wzZHUifQ.ZIXQ4cl0l5C5UNYuuJtTzw', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox://styles/mapbox/streets-v10',
+    accessToken: 'pk.eyJ1Ijoiaml1bnR3YW5nIiwiYSI6ImNpeWs5OWVvMjAwMHYyd25yNnh5b2wzZHUifQ.ZIXQ4cl0l5C5UNYuuJtTzw'
+});
+
+  
+
+// Map default position, coordinates, zoom, and layers
+
+var myMap = L.map('map-container', {
+    center: [46.852, -121.760],
+    zoom: 9,
+    layers: [satelliteLayer, defaultLayer]
+});
+
+
+
+// Location Markers
+  
+  L.marker([46.853, -121.760]).addTo(myMap)
+  .bindPopup('Welcome to Mt. Rainier! This peak is 4,392 feet high.');
+  
+  L.marker([47.0137235,-121.38759]).addTo(myMap)
+  .bindPopup('Norse Peak Wilderness');
+  
+  L.marker([46.7859626,-121.7369366]).addTo(myMap)
+  .bindPopup('Skyline Loop Trailhead');
+  
+  L.marker([46.7859626,-121.7435026]).addTo(myMap)
+  .bindPopup('Myrtle Falls');
+  
+  L.marker([46.8240324,-121.8631506]).addTo(myMap)
+  .bindPopup('Liberty Cap');
+  
+  
+
+  
+
+var mtnMaps = {
+    "Satellite": satelliteLayer,
+    "Default": defaultLayer,
+};  
+   
+
+// Controls
+
+L.control.layers(mtnMaps).addTo(myMap);   
+
+
+});
+
+
+//MAP 2
+
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+
+var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+var mapLayers = {
+    "Satellite": satLayer,
+    "Map View": drawLayer,
+    "Open Street Maps": osm
+}
+
+var map = L.map('map-container').setView([46.852, -121.760], 13);
+
+L.control.layers(mapLayers).addTo(map);
+satLayer.addTo(map);
+
+var marker = L.marker([46.852, -121.760]).addTo(map);
+marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
